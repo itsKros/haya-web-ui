dbb943afa830bc824c2615c3dab3523d40de818d
itsKros <kroshan197@gmail.com>
2019-12-20 19:20:22 +0530

    Home Page UI: region section complete and popular places section initialted

--- index.php
+++ index.php
@@ -15,16 +15,16 @@
 
 
 <!--Region section starts-->
-<section id="regionSec" class="in-grid py-5">
+<section id="regionSec" class="bg-gray-100 py-20 md:py-32">
   <div class="container mx-auto w-full content">
-      <div class="section-title text-center w-full">
+      <div class="section-title text-center w-full mb-20 md:mb-32">
         <h5 class="captiontitle">Welcome to Haya Lifestyle</h5>
         <h2 class="title">More Affordable Meals,<br> More <span>Offers</span></h2>
       </div>
   </div>
   <div class="container mx-auto flex flex-wrap flex-col md:flex-row w-5/6 content">
 
-    <div class="w-full md:w-2/5 mb-4 md:mx-2 flex flex-col md:flex-row flex-grow flex-shrink-0 box-shadow region-wrap">
+    <div class="w-full md:w-2/5 mb-4 md:mx-2 flex flex-col md:flex-row flex-grow flex-shrink-0 box-shadow bg-white region-wrap">
       <div class="w-full md:w-3/4 bg-cover bg-no-repeat img central" style="">
         <img src="assets/imgs/central.jpg" class="invisible" alt="" srcset="">
       </div>
@@ -35,7 +35,7 @@
       </div>
     </div>
 
-    <div class="w-full md:w-2/5 mb-4 md:mx-2 flex flex-col md:flex-row flex-grow flex-shrink-0 box-shadow region-wrap">
+    <div class="w-full md:w-2/5 mb-4 md:mx-2 flex flex-col md:flex-row flex-grow flex-shrink-0 box-shadow bg-white region-wrap">
       <div class="w-full md:w-3/4 bg-cover bg-no-repeat img east" style="">
         <img src="assets/imgs/east.jpg" class="invisible" alt="" srcset="">
       </div>
@@ -46,7 +46,7 @@
       </div>
     </div>
 
-    <div class="w-full md:w-2/5 mb-4 md:mx-2 flex flex-col md:flex-row flex-grow flex-shrink-0 box-shadow region-wrap">
+    <div class="w-full md:w-2/5 mb-4 md:mx-2 flex flex-col md:flex-row flex-grow flex-shrink-0 box-shadow bg-white region-wrap">
       <div class="w-full md:w-3/4 bg-cover bg-no-repeat img west" style="">
         <img src="assets/imgs/west.jpg" class="invisible" alt="" srcset="">
       </div>
@@ -57,20 +57,47 @@
       </div>
     </div>
 
-    <div class="w-full md:w-2/5 mb-4 md:mx-2 flex flex-col md:flex-row flex-grow flex-shrink-0 box-shadow region-wrap">
+    <div class="w-full md:w-2/5 mb-4 md:mx-2 flex flex-col md:flex-row flex-grow flex-shrink-0 box-shadow bg-white region-wrap">
       <div class="w-full md:w-3/4 bg-cover bg-no-repeat img north" style="">
         <img src="assets/imgs/north.jpg" class="invisible" alt="" srcset="">
       </div>
       <div class="content p-5">
         <h3 class="text-lg mb-2">North</h3>
         <p class="text-gray-500 text-sm mb-3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</p>
-        <a href="" class="kbtn transparent icon right  block w-1/4">Explore <i class="fas fa-arrow-right"></i></a>
+        <a href="" class="kbtn transparent icon right block w-1/4">Explore <i class="fas fa-arrow-right"></i></a>
       </div>
     </div>
     
   </div>
-  
 </section>
 <!--Region section ends-->
+
+
+
+<!--Popular Places section starts-->
+<section id="popularPlacesSec" class="container mx-auto w-full md:w-5/6 bg-white py-20 md:py-32">
+  <div class="w-full content">
+    <h5 class="text-gray-800 text-3xl mb-3">Popular <span class="text-teal-500">Places</span></h5>
+    <p class="text-gray-600 mb-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit, <br>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+  </div>
+
+  <div class="w-full flex flex-col md:flex-row box-shadow content">
+    <div class="w-full h-auto md:w-1/6 bg-cover bg-no-repeat img" style="background-image:url('assets/imgs/central.jpg')">
+      <img src="assets/imgs/west.jpg" class="invisible" alt="" srcset="">
+    </div>
+    <div class="w-full h-auto md:w-5/6 p-5">
+      <h3 class="text-lg mb-2">Kintamani Indonesian Restaurant</h3>
+      <p class="text-gray-500 text-sm mb-3">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+      </p>
+      <a href="" class="kbtn transparent icon right  block w-32 text-gray-800">View Offers <i class="fas fa-arrow-right"></i></a>
+    </div>
+  </div>
+
+</section>
+
+<!--Popular Places section ends-->
 
 <?php include('footer.php') ?>
